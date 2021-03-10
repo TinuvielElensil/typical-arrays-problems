460c25287a23f9ce4060705a88ada33fcec45d16
TinuvielElensil <elf-oracul@list.ru>
2021-03-10 03:19:47 +0300

    feat: add min, max and avg funcrions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) {
+    return 0
+  }
+  else if (array.length > 0) {
+    let min = array[0]
+    for (i = 0; i < array.length; i++) {
+      if (min > array[i]) min = array[i]
+    }
+    return min;
+  } return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined){
+    return 0
+  }
+  else if (array.length > 0) {
+    let max = array[0]
+    for (i = 0; i < array.length; i++) {
+      if (max < array[i]) max = array[i]
+    }
+    return max;
+  } return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) {
+    return 0
+  }
+  else if (array.length > 0) {
+    let avg = 0
+    let sum = 0
+    for (let i = 0; i < array.length; i ++) {
+      sum = sum + array[i]
+      avg = sum / array.length
+    } 
+  return avg;
+  } return 0
 }
